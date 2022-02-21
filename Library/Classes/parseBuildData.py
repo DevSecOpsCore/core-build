cda0298bc712e99fa5d5569b6f85c7261ff74d8a
enespekdas <enes.pekdas@gmail.com>
2022-02-21 13:58:36 +0300

    action.yaml steps changed v1.1.3

--- Library/Classes/parseBuildData.py
+++ Library/Classes/parseBuildData.py
@@ -52,7 +52,7 @@ class ParseBuildData:
                ParseBuildData.commandParameters['jName'] = args.jName
             else:
                 ParseBuildData.commandParameters['jName'] = args.jName+'.json'
-        if args.jPath is None:
+        if args.jPath is 'default':
             ParseBuildData.commandParameters['jPath'] = ''
         else:
             ParseBuildData.commandParameters['jPath'] = args.jPath
